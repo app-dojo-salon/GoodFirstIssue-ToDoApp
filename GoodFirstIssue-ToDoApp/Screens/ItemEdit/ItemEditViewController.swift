bd7b4a0ba5e3a62dc5e61bacf5021e1102afed65
turara <turara.developer@gmail.com>
2020-09-06 00:55:00 +0900

    ItemEditViewControllerを追加
    Screens/ItemEditにItemEditViewControllerを追加、Main.stroyboardでModal表示を設定

---
+++ GoodFirstIssue-ToDoApp/Screens/ItemEdit/ItemEditViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ItemEditViewController.swift
+//  GoodFirstIssue-ToDoApp
+//
+//  Created by Turara on 2020/09/06.
+//  Copyright © 2020 GoodFirstIssue. All rights reserved.
+//
+
+import UIKit
+
+class ItemEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "項目追加"
+    }
+    
+    
+}
