25e62d9c571e85331029f4da8bbf83bd4961db67
minmin0530 <izumiyoshiki@icloud.com>
2020-09-06 21:40:28 +0900

    ラベルとボタンとテキストフィールドを追加

--- GoodFirstIssue-ToDoApp/Screens/ItemEdit/ItemEditViewController.swift
+++ GoodFirstIssue-ToDoApp/Screens/ItemEdit/ItemEditViewController.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
-class ItemEditViewController: UIViewController {
+class ItemEditViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "項目追加"
+        nameTextField.delegate = self
     }
     
+    @IBAction func tapCancel(_ sender: Any) {
+        dismiss(animated: false)
+    }
     
+    @IBAction func tapSave(_ sender: Any) {
+        dismiss(animated: false)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if nameTextField.text != "" {
+            saveButton.isEnabled = true
+        }
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if nameTextField.text != "" {
+            saveButton.isEnabled = true
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.endEditing(true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameTextField.endEditing(true)
+    }
 }
