131293ada695f0a064823e6120bcafd3f08fce6f
turara <turara.developer@gmail.com>
2020-09-10 21:06:59 +0900

    Unwind segueを追加

--- GoodFirstIssue-ToDoApp/Screens/ItemList/ItemListViewController.swift
+++ GoodFirstIssue-ToDoApp/Screens/ItemList/ItemListViewController.swift
@@ -12,9 +12,8 @@ class ItemListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction private func exitDone(segue: UIStoryboardSegue) {}
 }
 
