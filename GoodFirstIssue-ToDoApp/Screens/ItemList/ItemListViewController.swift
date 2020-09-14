1ea034a62392d5cc5afe6b38ad3b730a3f5a1f17
Ryota Miyazaki <>
2020-09-14 20:39:26 +0900

    ItemCell.swiftとxibファイル、tableviewのDatasourceメソッド

--- GoodFirstIssue-ToDoApp/Screens/ItemList/ItemListViewController.swift
+++ GoodFirstIssue-ToDoApp/Screens/ItemList/ItemListViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ItemListViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.register(ItemCell.loadNib(), forCellReuseIdentifier: ItemCell.reuseIdentifier)
+        }
+    }
+    
+    var itemList: [(String)] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
+
+extension ItemListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ItemCell.reuseIdentifier, for: indexPath) as? ItemCell else { return UITableViewCell() }
+        cell.configure(name: itemList[indexPath.row], checked: false)
+        return cell
+    }
+    
+}
+
+extension ItemListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
 
