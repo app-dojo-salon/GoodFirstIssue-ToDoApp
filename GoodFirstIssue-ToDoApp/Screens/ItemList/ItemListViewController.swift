6e8f3ad3b60cc4783b9a0f6d6591e3cf27e309dc
akio0911 <akio0911@gmail.com>
2020-09-25 21:07:10 +0900

    Revert "Realmの実装 Part2です。"
    This reverts commit 7ce9e7858ada7dcb1e9161525b4cea784c4ba4ba.

--- GoodFirstIssue-ToDoApp/Screens/ItemList/ItemListViewController.swift
+++ GoodFirstIssue-ToDoApp/Screens/ItemList/ItemListViewController.swift
@@ -7,62 +7,12 @@
 //
 
 import UIKit
-import RealmSwift
 
 class ItemListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-//        let libraryPath = NSHomeDirectory() + "Library"
-//        let filePath = NSURL(fileURLWithPath: libraryPath + "/Test.realm")
-//
-//        //let realm = try! Realm(fileURL: filePath as URL)
-//
-//        //Realm書き込み
-//        let itemData = ItemModel()
-//        itemData.id = 7
-//        itemData.name = "apple"
-//        itemData.isCheck = false
-//        itemData.isDelet = true
-//
-//        try! realm.write {
-//            realm.add(itemData)
-//        }
-        
-        let itemData = ItemModel()
-
-//        itemData.id = 7
-        itemData.name = "apple"
-        itemData.isCheck = false
-        itemData.isDelet = true
-
-
-        
-        let realm = try! Realm()
-        try! realm.write {
-            
-            itemData.id += 1
-            realm.add(itemData)
-        }
-        
-        
-        
-        
-        let itemShow = realm.objects(ItemModel.self)
-        for item in itemShow{
-            print(item.id)
-            print(item.name)
-            print(item.isCheck)
-            print(item.isDelet)
-            
-        }
-        
-        
-        
-        
-        
     }
 
 
